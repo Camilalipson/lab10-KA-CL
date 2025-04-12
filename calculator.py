fa2096d57211dee1d52f65ea6468b7bce5fb3fb3
camilalipson <camilalipson@ufl.edu>
2025-04-12 15:17:31 -0400

    part 1 done

--- calculator.py
+++ calculator.py
@@ -1,26 +1,18 @@
 import math
-
-def square_root(a):
-    if a >= 0:
-        return math.sqrt(a)
-    else:
-        raise ValueError("Cannot calculate square root of negative numbers.")
-def hypotenuse(a,b):
-    return math.hypot(a,b)
 def add(a,b):
     return a+b
-def subtract(a,b):
+def sub(a,b):
     return a-b
-def multiply(a,b):
+def mul(a,b):
     return a*b
-def divide(a,b):
-    if a > 0:
-        return b/a
+def div(a,b):
+    if a ==0:
+        raise ValueError("ZeroDivisionError")
     else:
-        raise ValueError("Cannot divide by zero")
-def logarithm(a,b):
+        return b/a
+def log(a,b):
     if b <= 0 or a <= 1:
             raise ValueError("Base must be greater than 1 and Argument must be greater than 0")
     return math.log(b,a)
-def exponent(a,b):
+def exp(a,b):
     return a**b
